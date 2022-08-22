132c8f01001eed5693cf9ad5e21fa485c5ee0aec
lelemegan <tiongmegan@gmail.com>
2022-08-22 21:51:07 +1000

    edited and passed the test cases in ed

--- task_4.py
+++ task_4.py
@@ -1,4 +1,4 @@
-playersss = [{'name': 'Player 1', 'score': 20, 'stayed': False, 'at_14': True, 'bust': False},
+playersss = [{'name': 'Player 1', 'score': 20, 'stayed': True, 'at_14': True, 'bust': False},
            {'name': 'Player 2', 'score': 17, 'stayed': True, 'at_14': True, 'bust': False},
            {'name': 'Player 3', 'score': 19, 'stayed': True, 'at_14': True, 'bust': False},
            {'name': 'Player 4', 'score': 19, 'stayed': True, 'at_14': True, 'bust': False}]
@@ -14,36 +14,37 @@ def end_of_game(players):
     Returns True if round has ended or False if not. If true results are
     printed before return.
     """
-    #raise NotImplementedError
-    #one winner
-    #Player 1 is the winner
 
-    #draw
-
-    #bust
     stayed_and_not_busted = []
     who_busted = []
     for x in players:
-        if x["stayed"] == True: #if anyone decides to stay, return True 
-            return True
-        elif x["bust"] == False: #if anyone decides not to stay and is not busted, store score into list
+        #if anyone decides not to stay(continue playing)
+        #and this person is not busted
+        #return False (not end of game yet) 
+        if x["stayed"] == False and x["bust"] == False: 
+            return False
+        #if anyone decides to stay and is not busted
+        #store the score into list
+        elif x["stayed"] == True and x["bust"] == False: #if anyone decides not to stay and is not busted, store score into list
             stayed_and_not_busted.append(x["score"])
         elif x["bust"] == True:
             who_busted.append(x["name"])
 
+    if len(who_busted)==len(players):
+        print("The game is a draw! No one wins :(")
+        return True
+
     highest_score = max(stayed_and_not_busted)
     who_has_highest_score = []
-    for x in stayed_and_not_busted:
+    for x in players:
         if x["score"] == highest_score:
             who_has_highest_score.append(x["name"])
     
-    if len(who_has_highest_score)==1:
-        print(who_has_highest_score[0]+"is the winner!")
+    no_of_highest_score_players = len(who_has_highest_score)
+    if no_of_highest_score_players==1:
+        print(who_has_highest_score[0]+" is the winner!")
         return True
-    elif len(who_has_highest_score)>1:
-        print("The game is a draw! No one wins :(")
-        return True
-    elif len(who_busted)==len(players):
+    elif no_of_highest_score_players>1:
         print("The game is a draw! No one wins :(")
         return True
 
